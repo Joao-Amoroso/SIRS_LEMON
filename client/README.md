8dbc51f5ad11da70e8a464265c7ff5be4e9a00c4
Joao-Amoroso <joaoafonsoamoroso@gmail.com>
2022-12-08 01:30:33 +0000

    changed ui in  client app

---
+++ client/README.md
@@ -0,0 +1,22 @@
+# client app
+
+# to run
+
+In lemon directory
+
+```bash
+npm install
+```
+
+And to run
+
+```bash
+npx expo start
+```
+
+# technologies
+
+-   [expo](https://docs.expo.dev/)
+-   [react-native-maps](https://github.com/react-native-maps/react-native-maps)
+-   [expo-location](https://docs.expo.dev/versions/v47.0.0/sdk/location/)
+-   [react-native-svg](https://www.npmjs.com/package/react-native-svg#features)
--- client/lemon/App.js
+++ client/lemon/App.js
@@ -1,105 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableHighlight,
-    TextInput,
-    SafeAreaView
-} from "react-native";
-import { useState } from "react";
-import FirstStep from "./pages/firstStep";
-import SecondStep from "./pages/SecondStep";
-import ThirdStep from "./pages/ThirdStep";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./pages/home";
+import Register from "./pages/register";
+import Login from "./pages/login";
+const Stack = createNativeStackNavigator();
 export default function App() {
-    const [selected, setSelected] = useState(0);
     return (
-        <SafeAreaView style={styles.container}>
-            <Text
-                style={{ fontSize: 30, fontWeight: "bold", marginBottom: 40 }}
-            >
-                Rent a vehicle
-            </Text>
-            <View
-                style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    marginBottom: 40
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerShown: false
                 }}
             >
-                <View
-                    style={{
-                        backgroundColor: "red",
-                        alignItems: "center",
-                        borderRadius: 1000,
-                        width: 30,
-                        height: 30
-                    }}
-                >
-                    <TouchableHighlight onPress={() => setSelected(0)}>
-                        <Text style={{ fontSize: 20 }}>1</Text>
-                    </TouchableHighlight>
-                </View>
-                <View
-                    style={{
-                        width: 30,
-                        height: 1,
-                        backgroundColor: "gray",
-                        marginHorizontal: 4
-                    }}
-                ></View>
-                <View
-                    style={{
-                        backgroundColor: "red",
-                        alignItems: "center",
-                        borderRadius: 1000,
-                        width: 30,
-                        height: 30
-                    }}
-                >
-                    <TouchableHighlight onPress={() => setSelected(1)}>
-                        <Text style={{ fontSize: 20 }}>2</Text>
-                    </TouchableHighlight>
-                </View>
-                <View
-                    style={{
-                        width: 30,
-                        height: 1,
-                        backgroundColor: "gray",
-                        marginHorizontal: 4
-                    }}
-                ></View>
-                <View
-                    style={{
-                        backgroundColor: "red",
-                        alignItems: "center",
-                        borderRadius: 1000,
-                        width: 30,
-                        height: 30
-                    }}
-                >
-                    <TouchableHighlight onPress={() => setSelected(2)}>
-                        <Text style={{ fontSize: 20 }}>3</Text>
-                    </TouchableHighlight>
-                </View>
-            </View>
-            {selected == 0 ? (
-                <FirstStep />
-            ) : selected == 1 ? (
-                <SecondStep />
-            ) : (
-                <ThirdStep />
-            )}
-        </SafeAreaView>
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={Register}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        marginTop: 50
-    }
-});
