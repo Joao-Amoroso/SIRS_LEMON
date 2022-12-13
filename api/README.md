9f10f22de6b423b894ef86266f74ff6801a16bba
Joao-Amoroso <joaoafonsoamoroso@gmail.com>
2022-12-13 16:24:43 +0000

    ADD routes to readme and added petite vue for reactive ui

--- api/README.md
+++ api/README.md
@@ -1,6 +1,6 @@
-# API Server
+FORMAT: 1A
 
-# Installation
+# Setup
 
 In the **api** directory
 
@@ -48,8 +48,172 @@ if you want to run in debug mode write instead
 flask --debug run
 ```
 
+# Routes
+
+All routes that the api handles
+
+-   **[login](#get-login)** : `GET /login`
+-   **[register](#get-register)** : `GET /register`
+-   **[home](#get])** : `GET /`
+-   **[rent](#post-rent)** : `POST /rent`
+-   **[get vehicles unlocked](#get-vehiclesunlocked)** : `GET /vehicles/unlocked`
+
+# GET /login
+
+Renders a login page
+
+## Response
+
+### **code**: `200 OK`
+
+**returns**: [`login.html`](./templates/login.html)
+
+# GET /register
+
+Renders a register page
+
+## Response
+
+### **code**: `200 OK`
+
+**returns**: [`resgister.html`](./templates/register.html)
+
+# GET /
+
+Renders the home page
+
+## **Auth required**
+
+## Response
+
+### **code**: `200 OK`
+
+**returns**: [`index.html`](./templates/index.html)
+
+### **code**: `401 Unauthorized`
+
+Redirects to login page
+
+# POST /rent
+
+Rent a vehicle
+
+## **Auth required**
+
+## Request
+
+Data Constraints
+
+```json
+{
+    "vehicleid": "[valid vehicle id]",
+    "duration": "[valid duration in minutes]",
+    "payment_type": "[credit or debit ]",
+    "name": "[name on card]",
+    "card_number": "[card number]",
+    "expiration": "[MM/YY]",
+    "cvv": "[CVV]"
+}
+```
+
+Example
+
+```json
+{
+    "vehicleid": "vyh345",
+    "duration": 360,
+    "payment_type": "credit",
+    "name": "Alberto Maria Pereira Gomes",
+    "card_number": "9999 9999 9999 9999",
+    "expiration": "12/12",
+    "cvv": "123"
+}
+```
+
+## Response
+
+### **code**: `200 OK`
+
+### **code**: `400 BAD REQUEST`
+
+### **returns**
+
+```json
+{
+    "errors": [
+        {
+            "field": "[header]",
+            "error": "error"
+        }
+    ]
+}
+```
+
+Example
+
+```json
+{
+    "errors": [
+        {
+            "field": "vehicleid",
+            "error": "The vehicle vyh346 doesn't exist."
+        },
+        {
+            "field": "card",
+            "error": "The card doesn't exist."
+        }
+    ]
+}
+```
+
+### **code**: `401 Unauthorized`
+
+Redirects to login page
+
+# GET /vehicles/unlocked
+
+Returns all vehicles that are unlocked
+
+## **Auth required** (Need to be an employee)
+
+## Response
+
+### **code**: `200 OK`
+
+```json
+[
+    {
+        "vehicleid": "[]",
+        "lat": "[]",
+        "lgt": "[]"
+    }
+]
+```
+
+Example
+
+```json
+[
+    {
+        "vehicleid": "vyh345",
+        "lat": "38.705077",
+        "lgt": "-9.385283"
+    },
+    {
+        "vehicleid": "vyh344",
+        "lat": "38.745548",
+        "lgt": "-9.147874"
+    }
+]
+```
+
+### **code**: `401 Unauthorized`
+
+Redirects to login page
+
 # dependencies
 
 -   psycopg2
 -   flask
 -   flask_bcrypt
+-   python-dotenv
--- api/app.py
+++ api/app.py
@@ -3,28 +3,30 @@ import psycopg2.extras
 from flask import Flask, render_template, request, make_response, session, redirect, url_for
 import json
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # SGBD configs
-DB_HOST = ""
-DB_USER = ""
-DB_DATABASE = ""
-DB_PASSWORD = ""
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_DATABASE = os.environ.get("DB_DATABASE")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (
     DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD)
 
 app = Flask(__name__)
-app.secret_key = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-bcrypt = Bcrypt(app)
+# app.secret_key = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+# bcrypt = Bcrypt(app)
 
 
 @app.route("/")
 def hello_world():
-    # if "user" not in session:
-    #     return redirect(url_for("login"))
-
     return render_template('index.html')
 
 
-@app.route('/vehicles')
+@app.route('/vehicles/unlocked')
 def get_vehicles():
     dbConn = None
     cursor = None
@@ -83,87 +85,86 @@ def rent():
         dbConn.close()
 
 
-@app.route('/login', methods=["POST", "GET"])
+@app.route('/login', methods=["GET"])
 def login():
-    if request.method == "GET":
-        return render_template('login.html')
-    # body {username,password}
-    dbConn = None
-    cursor = None
-    try:
-        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
-        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        body = request.get_json()
-        query = """ SELECT * from client where username = $1;
-"""
 
-        data = (body["username"],)
-        cursor.execute(query, data)
+    return render_template('login.html')
+#     # body {username,password}
+#     dbConn = None
+#     cursor = None
+#     try:
+#         dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+#         cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#         body = request.get_json()
+#         query = """ SELECT * from client where username = $1;
+# """
 
-        record = cursor.fetchone()
+#         data = (body["username"],)
+#         cursor.execute(query, data)
 
-        if not record:
-            return json.dumps({'message': "The username doesn't exist", 'code': 401})
+#         record = cursor.fetchone()
 
-        # hash password
-        hash_password = bcrypt.generate_password_hash(body["password"])
+#         if not record:
+#             return json.dumps({'message': "The username doesn't exist", 'code': 401})
 
-        if bcrypt.check_password_hash(hash_password, record[0]):
-            return json.dumps({'message': "The password isn't correct", 'code': 401})
+#         # hash password
+#         hash_password = bcrypt.generate_password_hash(body["password"])
 
-        # generate cookie
-        cookie = "boas"
-        resp = make_response(render_template('<h1>cookie was set</h1>'))
-        resp.set_cookie('userID', cookie)
+#         if bcrypt.check_password_hash(hash_password, record[0]):
+#             return json.dumps({'message': "The password isn't correct", 'code': 401})
 
-        return resp
-    except Exception as e:
-        return json.dumps({'error': str(e)})
-    finally:
-        dbConn.commit()
-        cursor.close()
-        dbConn.close()
+#         # generate cookie
+#         cookie = "boas"
+#         resp = make_response(render_template('<h1>cookie was set</h1>'))
+#         resp.set_cookie('userID', cookie)
+
+#         return resp
+#     except Exception as e:
+#         return json.dumps({'error': str(e)})
+#     finally:
+#         dbConn.commit()
+#         cursor.close()
+#         dbConn.close()
 
 
-@app.route('/register', methods=["POST", "GET"])
+@app.route('/register', methods=["GET"])
 def register():
-    # body {username,password}
-    if request.method == "GET":
-        return render_template('register.html')
-    dbConn = None
-    cursor = None
-    try:
-        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
-        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        body = request.get_json()
-        query = """ SELECT * from client where username = $1;
-"""
 
-        data = (body["username"],)
-        cursor.execute(query, data)
+    return render_template('register.html')
+#     dbConn = None
+#     cursor = None
+#     try:
+#         dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+#         cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#         body = request.get_json()
+#         query = """ SELECT * from client where username = $1;
+# """
 
-        record = cursor.fetchone()
+#         data = (body["username"],)
+#         cursor.execute(query, data)
 
-        if record:
-            return json.dumps({'message': "The username already exists", 'code': 401})
+#         record = cursor.fetchone()
 
-        # hash password
-        hash_password = bcrypt.generate_password_hash(body["password"])
-        query = """ insert into client values ($1,$2);
-"""
+#         if record:
+#             return json.dumps({'message': "The username already exists", 'code': 401})
 
-        data = (body["username"], hash_password)
-        cursor.execute(query, data)
+#         # hash password
+#         hash_password = bcrypt.generate_password_hash(body["password"])
+#         query = """ insert into client values ($1,$2);
+# """
 
-        # generate cookie
-        cookie = "boas"
-        resp = make_response(render_template('<h1>cookie was set</h1>'))
-        resp.set_cookie('userID', cookie)
+#         data = (body["username"], hash_password)
+#         cursor.execute(query, data)
 
-        return resp
-    except Exception as e:
-        return json.dumps({'error': str(e)})
-    finally:
-        dbConn.commit()
-        cursor.close()
-        dbConn.close()
+#         # generate cookie
+#         cookie = "boas"
+#         resp = make_response(render_template('<h1>cookie was set</h1>'))
+#         resp.set_cookie('userID', cookie)
+
+#         return resp
+#     except Exception as e:
+#         return json.dumps({'error': str(e)})
+#     finally:
+#         dbConn.commit()
+#         cursor.close()
+#         dbConn.close()
