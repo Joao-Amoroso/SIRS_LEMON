c021af21f227a336cc0353e8b355649f57ca2218
JohnFox191 <joaopedrosoraposo@gmail.com>
2022-12-30 16:51:27 +0000

    Set flask port as 80

--- api/app.py
+++ api/app.py
@@ -268,4 +268,4 @@ def register():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=80)
--- authserver/app.py
+++ authserver/app.py
@@ -140,6 +140,6 @@ def register():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=80)
 
     
